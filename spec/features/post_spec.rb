f686f47526276f88dc371685737c05214cab473c
compostbrain <joel@wildmeadowsfarm.com>
2017-08-18 16:32:31 -0400

    Refactored index page to user Post partial

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-     user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Doe")
-      login_as(user, :scope => :user)
+     @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Doe")
+      login_as(@user, :scope => :user)
   end
+
   describe 'index' do
     before do
       visit posts_path
@@ -18,13 +19,13 @@ describe 'navigate' do
     end
 
     it 'has a list of posts' do
-      post1 = Post.create(date: Date.today, rationale: "Post1")
-      post2 = Post.create(date: Date.today, rationale: "Post2")
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
       visit posts_path
       expect(page).to have_content(/Post1|Post2/)
     end
   end
-
+  
   describe "creation" do
     before do
       visit new_post_path
