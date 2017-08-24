596a6eb19b7d634188f9c59a61185ebe5f4b05cd
compostbrain <joel@wildmeadowsfarm.com>
2017-08-24 17:48:32 -0400

    implemented overtime hours to posts with validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Admin")
  puts "1 Admin-user created"
  100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts created."
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :post do
     date Date.today
     rationale "Some Rationale"
+    overtime_request 3.5
 
     user
   end
@@ -9,7 +10,7 @@ FactoryGirl.define do
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "Some more content"
-
+    overtime_request 0.5
     user
   end
   factory :post_from_another_user, class: "Post" do
