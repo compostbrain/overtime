56889d44d0a6d223ce52469c1d84d2bd3d618e4f
compostbrain <joel@wildmeadowsfarm.com>
2017-08-30 21:35:36 -0400

    Added phone attribute to User and made it required

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
- @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Doe")
- puts "1 user created"
-AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Admin")
- puts "1 Admin-user created"
- 100.times do |post|
+@user = User.create(
+                    email: 'test@test.com',
+                    password: 'asdfasdf',
+                    password_confirmation: 'asdfasdf',
+                    first_name: 'John',
+                    last_name: 'Doe',
+                    phone: '9523143993'
+                     )
+
+puts '1 user created'
+
+AdminUser.create(
+                 email: 'admin@test.com',
+                 password: 'asdfasdf',
+                 password_confirmation: 'asdfasdf',
+                 first_name: 'John',
+                 last_name: 'Admin',
+                 phone: '9523143993'
+                 )
+puts '1 Admin-user created'
+100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
-puts "100 posts created."
+puts '100 posts created.'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,11 +11,23 @@ RSpec.describe User, type: :model do
     end
     it "cannot be created without first_name, last_name" do
       @user.first_name = nil
+
+      expect(@user).to_not be_valid
+    end
+    it "cannot be created without first_name, last_name" do
+
       @user.last_name = nil
+      
+      expect(@user).to_not be_valid
+    end
+    it "cannot be created without first_name, last_name" do
+
+
+      @user.phone = nil
       expect(@user).to_not be_valid
     end
 
-    
+
   end
   describe "custom name nethods" do
     it 'has a full name method that combines first and last name' do
