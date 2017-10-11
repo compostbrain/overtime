c3f128bdf6944489188144c63f04244e9515547b
compostbrain <joelcahalan@gmail.com>
2017-10-11 11:12:46 -0400

    Implemented initial view code for audit logs

---
+++ spec/features/audit_log_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe  "Audit Log Feature" do
+  let! :audit_log { FactoryGirl.create(:audit_log) }
+
+  describe "index" do
+    before do
+      admin_user = FactoryGirl.create(:admin_user)
+      login_as(admin_user, :scope => :user)
+    end
+    it 'has an index page that can be reached' do
+      visit audit_logs_path
+      expect(page.status_code).to eq(200)
+    end
+    it 'renders audit log content' do
+      visit audit_logs_path
+      expect(page).to have_content(/DOE, JOHN/)
+    end
+    xit "cannot be accessed by non-admin users" do
+
+    end
+  end
+end
