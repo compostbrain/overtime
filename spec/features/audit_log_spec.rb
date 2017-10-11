fa5ee90572d491d4bea4cf2998a6744331277043
compostbrain <joelcahalan@gmail.com>
2017-10-11 11:17:07 -0400

    Blcoked non-admin users from accessing indexpage for audit logs

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -16,8 +16,13 @@ describe  "Audit Log Feature" do
       visit audit_logs_path
       expect(page).to have_content(/DOE, JOHN/)
     end
-    xit "cannot be accessed by non-admin users" do
+    it "cannot be accessed by non-admin users" do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, scope: :user)
 
+      visit audit_logs_path
+      expect(current_path).to eq(root_path)
     end
   end
 end
